4d6c604dc01ff2d615b30a92d1d101be516b141d
Colin Stell <colinjstell@gmail.com>
2018-09-06 10:49:25 -0700

    mute and unmute func plus styling controls

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ let video = videoPlayer.querySelector('.video');
 let playPause = videoPlayer.querySelector('#play-pause-button');
 let playPauseIcons = playPause.getElementsByTagName('i')[0];
 let progress = videoPlayer.querySelector('.progress');
+let mute = videoPlayer.querySelector('.mute_button');
+let muteIcon = mute.getElementsByTagName('i')[0];
 
 
 //Progress Bar Function & Event Listener
@@ -13,8 +15,23 @@ let progressUpdate = () => {
         playPauseIcons.className = "fas fa-play-circle";
     }
 }
-
 video.addEventListener('timeupdate', progressUpdate);
+
+// Mute/Unmute Function & Event Listener 
+
+let muteVideo = () => {
+    if (video.muted) {
+        video.muted = false;
+        muteIcon.className = "fas fa-volume-up";
+    } else {
+        video.muted = true;
+        muteIcon.className = "fas fa-volume-off";
+    }
+}
+mute.addEventListener('click', muteVideo);
+
+// Volume Function & Event Listener
+
 
 
 // Play/Pause Function & Event Listener + Icon Change
@@ -27,6 +44,6 @@ let playPauseToggle = () => {
         playPauseIcons.className = "fas fa-play-circle";
     }
 }
-
 playPause.addEventListener('click', playPauseToggle);
 
+// Full Screen Function & Event Listener
