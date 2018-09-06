94f388fe49905bb37f24ebb2010d41301be6e5f4
Colin Stell <colinjstell@gmail.com>
2018-09-05 19:58:46 -0700

    additional JS functionality

--- js/main.js
+++ js/main.js
@@ -1,28 +1,32 @@
 let videoPlayer = document.querySelector('.video_player');
 let video = videoPlayer.querySelector('.video');
 let playPause = videoPlayer.querySelector('#play-pause-button');
-let playPauseIcons = playPause.getElementsByTagName('i')[0].getAttribute('class');
-console.log(playPauseIcons);
+let playPauseIcons = playPause.getElementsByTagName('i')[0];
+let progress = videoPlayer.querySelector('.progress');
 
-// Play/Pause Function & Event Listener
-let playPauseToggle = () => {
-    if(video.paused) {
-        video.play();
-    } else {
-        video.pause();
-    }
-}
 
-playPause.addEventListener('click', playPauseToggle);
-
-// Change Play/Pause Icons
-let playPauseIconToggle = () => {
-    if (video.paused) {
-        playPauseIcons.className = "far fa-pause-circle";
-    } else {
+//Progress Bar Function & Event Listener
+let progressUpdate = () => {
+    let videoProgress = video.currentTime / video.duration;
+    progress.style.width = videoProgress * 100 + '%';
+    if (video.ended){
         playPauseIcons.className = "fas fa-play-circle";
     }
 }
 
-playPause.addEventListener('click', playPauseIconToggle);
+video.addEventListener('timeupdate', progressUpdate);
+
+
+// Play/Pause Function & Event Listener + Icon Change
+let playPauseToggle = () => {
+    if(video.paused) {
+        video.play();
+        playPauseIcons.className = "far fa-pause-circle";
+    } else {
+        video.pause();
+        playPauseIcons.className = "fas fa-play-circle";
+    }
+}
+
+playPause.addEventListener('click', playPauseToggle);
 
