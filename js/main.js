efe864b81716dbe8611f6d757cdec753e6ebca6e
Colin Stell <colinjstell@gmail.com>
2018-09-07 13:07:20 -0700

    finished seek, highlight, text jump functionalities

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ let videoPlayer = document.querySelector('.video_player');
 let video = videoPlayer.querySelector('.video');
 let playPause = videoPlayer.querySelector('#play-pause-button');
 let playPauseIcons = playPause.getElementsByTagName('i')[0];
+let progressBar = videoPlayer.querySelector('.progress_bar');
 let progress = videoPlayer.querySelector('.progress');
 let mute = videoPlayer.querySelector('.mute_button');
 let muteIcon = mute.getElementsByTagName('i')[0];
@@ -11,6 +12,7 @@ let fullscreen = videoPlayer.querySelector('.fullscreen');
 let current = videoPlayer.querySelector('.current');
 let duration = videoPlayer.querySelector('.duration');
 let textData = document.getElementsByClassName('textSec');
+let textPar = document.querySelector('.textHighlight').getElementsByTagName('span');
 
 //Progress Bar Function & Event Listener + Current Time & Duration
 let progressUpdate = () => {
@@ -19,24 +21,43 @@ let progressUpdate = () => {
     if (video.ended){
         playPauseIcons.className = "fas fa-play-circle";
     }
+
+    // Click on Progress Bar and jump to that
+    progressBar.addEventListener('click', (event) => {
+        let percent = event.offsetX / progressBar.offsetWidth;
+        video.currentTime = percent * parseInt(video.duration).toFixed(2);
+        progress.style.width = percent / 100;
+      });
+
     // Time Tracker for Current Time & Duration Left
     let timeCurrent = parseFloat(video.currentTime).toFixed(2);
     current.innerHTML = timeCurrent;
     let timeDuration = parseFloat(video.duration).toFixed(2);
     duration.innerHTML = (timeDuration) - (timeCurrent);
     // Highlight Text that Matches current time
-    for (let i = 0; i < textData.length; i ++) {
-        // console.log(textData[i]);
-        console.log(textData[i].getAttribute('data-start'));
-        console.log(timeCurrent);
-        if (timeCurrent > textData[i].getAttribute('data-start')  && timeCurrent < textData[i].getAttribute('data-end')) {
-            textData[i].classList.add("hightlight");
+    for (let i = 0; i < textData.length; i++) {
+        if (video.currentTime > textData[i].getAttribute('data-start') && video.currentTime < textData[i].getAttribute('data-end')) {
+            textData[i].style.backgroundColor = "rgb(0, 234, 215)" ;
         } else {
-            textData[i].classList.remove('highlight');
+            textData[i].style.backgroundColor = "";
         }
     }
+
 }
 video.addEventListener('timeupdate', progressUpdate);
+
+
+
+
+// Time Jump Functionality when a user clicks on a section in the text
+
+for (let i = 0; i < textPar.length; i++) {
+    textPar[i].addEventListener('click', () => {
+        var timeJump = textPar[i].getAttribute('data-start');
+        video.currentTime = timeJump;
+        video.play();
+    });
+}
 
 // Mute/Unmute Function & Event Listener 
 
